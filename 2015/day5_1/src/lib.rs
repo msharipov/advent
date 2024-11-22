e571abcc764fce26f4881105a4a95a41e2ccb532
msharipov <msharipovr@gmail.com>
2024-11-22 14:17:07 -0500

    (15_5_1): check for double letters

--- 2015/day5_1/src/lib.rs
+++ 2015/day5_1/src/lib.rs
@@ -9,6 +9,12 @@ fn contains_enough_vowels(s: &str) -> bool {
     vowel_count >= 3
 }
 
+fn contains_double_letter(s: &str) -> bool {
+    s.chars()
+        .zip(s.chars().skip(1))
+        .any(|pair| pair.0 == pair.1)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -21,5 +27,15 @@ mod tests {
     #[test]
     fn contains_enough_vowels_test_2() {
         assert!(!contains_enough_vowels("cmppptelxclwlxz"));
+    }
+
+    #[test]
+    fn contains_double_letter_test_1() {
+        assert!(contains_double_letter("agafdgivdfqwee"));
+    }
+
+    #[test]
+    fn contains_double_letter_test_2() {
+        assert!(!contains_double_letter("aghpngertgbrtnbv"));
     }
 }
