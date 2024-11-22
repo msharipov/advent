620651797739bd1b543c233a4fa703fa1af391d3
msharipov <msharipovr@gmail.com>
2024-11-22 11:04:37 -0500

    (15_3_1): count visited houses

--- 2015/day3_1/src/lib.rs
+++ 2015/day3_1/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 #[derive(Debug, PartialEq)]
 pub enum Move {
     Up,
@@ -7,13 +9,36 @@ pub enum Move {
 }
 
 fn parse_moves(s: &str) -> Vec<Move> {
-    s.chars().filter_map(|c| match c {
-        '^' => Some(Move::Up),
-        '>' => Some(Move::Right),
-        'v' => Some(Move::Down),
-        '<' => Some(Move::Left),
-        _ => None,
-    }).collect()
+    s.chars()
+        .filter_map(|c| match c {
+            '^' => Some(Move::Up),
+            '>' => Some(Move::Right),
+            'v' => Some(Move::Down),
+            '<' => Some(Move::Left),
+            _ => None,
+        })
+        .collect()
+}
+
+fn next_pos(current: (i64, i64), dir: &Move) -> (i64, i64) {
+    let (x, y) = current;
+    match dir {
+        Move::Up => (x, y + 1),
+        Move::Down => (x, y - 1),
+        Move::Left => (x - 1, y),
+        Move::Right => (x + 1, y),
+    }
+}
+
+fn count_distinct_houses(moves: &[Move]) -> usize {
+    let mut visited = HashSet::new();
+    let mut pos = (0i64, 0i64);
+    visited.insert(pos);
+    for m in moves {
+        pos = next_pos(pos, m);
+        visited.insert(pos);
+    }
+    visited.len()
 }
 
 #[cfg(test)]
@@ -24,5 +49,10 @@ mod tests {
     #[test]
     fn parse_moves_test_1() {
         assert_eq!(parse_moves("^vb<>8"), vec![Up, Down, Left, Right]);
+    }
+
+    #[test]
+    fn count_distinct_houses_test_1() {
+        assert_eq!(count_distinct_houses(&parse_moves("^>v<>")), 4);
     }
 }
