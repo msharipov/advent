5066f232caf805a5ba8e6a89a03bf49a730e4341
msharipov <msharipovr@gmail.com>
2024-11-22 10:15:54 -0500

    (15_2_2): completed

--- 2015/day2_2/src/main.rs
+++ 2015/day2_2/src/main.rs
@@ -1,12 +1,12 @@
 use std::fs::read_to_string;
 
-use day2_1::Sides;
+use day2_2::Sides;
 
 fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
     let total: u32 = input
         .lines()
-        .map(|l| l.parse::<Sides>().unwrap().needed_paper())
+        .map(|l| l.parse::<Sides>().unwrap().needed_ribbon())
         .sum();
-    println!("{total} sq. feet of wrapping paper required");
+    println!("{total} feet of ribbon required");
 }
