085880a986bb413a20564dec7b643c6bbdaebc77
msharipov <msharipovr@gmail.com>
2024-11-23 17:01:57 -0500

    (15_6_1): parse grid actions from string

--- 2015/day6_1/src/lib.rs
+++ 2015/day6_1/src/lib.rs
@@ -1,17 +1,19 @@
 use sscanf::sscanf;
 use std::error::Error;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 struct Point {
     x: usize,
     y: usize,
 }
 
+#[derive(Debug, PartialEq)]
 struct Rect {
     corner_1: Point,
     corner_2: Point,
 }
 
+#[derive(Debug, PartialEq)]
 enum GridAction {
     On(Rect),
     Off(Rect),
@@ -19,8 +21,6 @@ enum GridAction {
 }
 
 const GRID_SIZE: usize = 1000;
-
-struct ActionError;
 
 impl Point {
     pub fn parse(s: &str) -> Result<Point, &'static str> {
@@ -36,13 +36,39 @@ impl Point {
     }
 }
 
-pub fn parse_action(s: &str) -> Result<GridAction, ActionError> {
+impl Rect {
+    pub fn new(p1: &Point, p2: &Point) -> Self {
+        let mut first: &Point = p1;
+        let mut second: &Point = p2;
+        if p2.x < p1.x || (p2.x == p1.x && p2.y < p1.y) {
+            first = p2;
+            second = p1;
+        }
+        Rect {
+            corner_1: (*first).clone(),
+            corner_2: (*second).clone(),
+        }
+    }
+}
+
+fn parse_action(s: &str) -> Result<GridAction, &'static str> {
     let tokens: Vec<_> = s.split(' ').collect();
-    match tokens.len() {
-        4 | 5 => {}
-        _ => return Err(ActionError),
-    };
-    todo!();
+    if tokens.len() == 5 && tokens[0] == "turn" {
+        let corner_1 = Point::parse(tokens[2])?;
+        let corner_2 = Point::parse(tokens[4])?;
+        let rect = Rect::new(&corner_1, &corner_2);
+        match tokens[1] {
+            "on" => return Ok(GridAction::On(rect)),
+            "off" => return Ok(GridAction::Off(rect)),
+            _ => return Err("invalid token after turn"),
+        }
+    } else if tokens.len() == 4 && tokens[0] == "toggle" {
+        let corner_1 = Point::parse(tokens[1])?;
+        let corner_2 = Point::parse(tokens[3])?;
+        let rect = Rect::new(&corner_1, &corner_2);
+        return Ok(GridAction::Toggle(rect));
+    }
+    Err("invalid command")
 }
 
 #[cfg(test)]
@@ -62,5 +88,33 @@ mod tests {
     #[test]
     fn point_parse_test_3() {
         assert_eq!(Err("point out of bounds"), Point::parse("1354,90"));
+    }
+
+    #[test]
+    fn parse_action_test_1() {
+        assert_eq!(Ok(GridAction::Toggle(Rect{
+            corner_1: Point {
+                x: 567,
+                y: 800,
+            },
+            corner_2: Point {
+                x: 975,
+                y: 23,
+            }
+        })), parse_action("toggle 975,23 through 567,800"))
+    }
+
+    #[test]
+    fn parse_action_test_2() {
+        assert_eq!(Ok(GridAction::On(Rect{
+            corner_1: Point {
+                x: 111,
+                y: 504,
+            },
+            corner_2: Point {
+                x: 111,
+                y: 877,
+            }
+        })), parse_action("turn on 111,877 through 111,504"))
     }
 }
