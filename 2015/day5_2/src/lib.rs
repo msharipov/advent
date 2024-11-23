bbb8275c3988aeab2aedf7aedf9c5ae64bab562b
msharipov <msharipovr@gmail.com>
2024-11-23 15:11:47 -0500

    (15_5_2): check if string contains a char sandwich

--- 2015/day5_2/src/lib.rs
+++ 2015/day5_2/src/lib.rs
@@ -4,24 +4,14 @@ fn contains_double_pair(s: &str) -> bool {
     pairs.any(|pair| other_pairs.any(|other| other == pair))
 }
 
-fn contains_double_letter(s: &str) -> bool {
+fn contains_sandwich(s: &str) -> bool {
     s.chars()
-        .zip(s.chars().skip(1))
+        .zip(s.chars().skip(2))
         .any(|pair| pair.0 == pair.1)
 }
 
-fn no_naughty_substrings(s: &str) -> bool {
-    const NAUGHTY: &[&str] = &["ab", "cd", "pq", "xy"];
-    for n in NAUGHTY {
-        if s.contains(n) {
-            return false;
-        }
-    }
-    true
-}
-
 pub fn is_nice(s: &str) -> bool {
-    contains_double_pair(s) && contains_double_letter(s) && no_naughty_substrings(s)
+    contains_double_pair(s) && contains_sandwich(s)
 }
 
 #[cfg(test)]
@@ -37,23 +27,14 @@ mod tests {
     fn contains_double_pair_test_2() {
         assert!(!contains_double_pair("aaaunotgbwrtuvnb"));
     }
+
     #[test]
-    fn contains_double_letter_test_1() {
-        assert!(contains_double_letter("agafdgivdfqwee"));
+    fn contains_sandwich_test_1() {
+        assert!(contains_sandwich("vsdfonouvfgjndghf"));
     }
 
     #[test]
-    fn contains_double_letter_test_2() {
-        assert!(!contains_double_letter("aghpngertgbrtnbv"));
-    }
-
-    #[test]
-    fn no_naughty_substrings_test_1() {
-        assert!(no_naughty_substrings("bvnqoerinvdsnfvj"));
-    }
-
-    #[test]
-    fn no_naughty_substrings_test_2() {
-        assert!(!no_naughty_substrings("abcdefghijklmnop"));
+    fn contains_sandwich_test_2() {
+        assert!(!contains_sandwich("bfgnsuinrfguerbv"));
     }
 }
