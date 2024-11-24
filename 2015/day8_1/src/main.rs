de0a8e23e303e45fb4fe28cea3ca24db88f97434
msharipov <msharipovr@gmail.com>
2024-11-24 01:04:48 -0500

    (15_8_1): finished

--- 2015/day8_1/src/main.rs
+++ 2015/day8_1/src/main.rs
@@ -1,7 +1,13 @@
+use day8_1::count_chars;
 use std::fs::read_to_string;
 
 fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
     let input: Vec<_> = input.trim().split('\n').collect();
-    println!("Hello, world!");
+    let code_count = input.iter().map(|l| l.len()).sum::<usize>();
+    let char_count = input
+        .iter()
+        .map(|l| count_chars(l).unwrap())
+        .sum::<usize>();
+    println!("difference = {}", code_count - char_count);
 }
