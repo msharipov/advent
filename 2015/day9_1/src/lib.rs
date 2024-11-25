52c44e93567fa0261eb228a33c97243fe006c7b1
msharipov <msharipovr@gmail.com>
2024-11-25 00:44:08 -0500

    (15_9_1): store distances in a GPS struct

--- 2015/day9_1/src/lib.rs
+++ 2015/day9_1/src/lib.rs
@@ -1,3 +1,8 @@
+use std::{
+    cmp::{max, min},
+    collections::HashMap,
+};
+
 use sscanf::sscanf;
 
 #[derive(Debug, PartialEq)]
@@ -7,17 +12,59 @@ struct Distance {
     dist: u64,
 }
 
+#[derive(Debug, Default)]
+struct GPS {
+    map: HashMap<(String, String), u64>,
+}
+
 impl Distance {
     pub fn new(s: &str) -> Result<Self, &'static str> {
         let parsed = match sscanf!(s, "{String} to {String} = {u64}") {
             Ok(p) => Ok(p),
             Err(_) => Err("cannot parse distance"),
         }?;
+        if parsed.0 == parsed.1 {
+            return Err("departure and destination are the same");
+        }
         Ok(Distance {
             from: parsed.0,
             to: parsed.1,
             dist: parsed.2,
         })
+    }
+}
+
+impl GPS {
+    fn add_distance(&mut self, dist: &Distance) {
+        let Distance {
+            from: a,
+            to: b,
+            dist: separation,
+        } = dist;
+
+        if a < b {
+            self.map.insert((a.to_owned(), b.to_owned()), *separation);
+        } else {
+            self.map.insert((b.to_owned(), a.to_owned()), *separation);
+        }
+    }
+
+    pub fn new(distances: &[&str]) -> Result<Self, &'static str> {
+        let mut gps = GPS::default();
+        for dist in distances {
+            match Distance::new(dist) {
+                Ok(d) => gps.add_distance(&d),
+                Err(e) => return Err(e),
+            }
+        }
+        Ok(gps)
+    }
+
+    pub fn get(&self, points: (&str, &str)) -> Option<&u64> {
+        self.map.get(&(
+            min(points.0, points.1).to_owned(),
+            max(points.0, points.1).to_owned(),
+        ))
     }
 }
 
@@ -43,5 +90,17 @@ mod tests {
             Distance::new("Coruscant to Shrek's Cave = Far Far Away"),
             Err("cannot parse distance")
         );
+    }
+
+    #[test]
+    fn gps_get_test_1() {
+        let gps = GPS::new(&["Springfield to Chicago = 202"]).unwrap();
+        assert_eq!(gps.get(("Springfield", "Chicago")), Some(&202));
+    }
+
+    #[test]
+    fn gps_get_test_2() {
+        let gps = GPS::new(&["Springfield to Chicago = 202", "Detroit to Chicago = 383"]).unwrap();
+        assert_eq!(gps.get(("Springfield", "Detroit")), None);
     }
 }
