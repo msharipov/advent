41f4f2679c43a7169fd32228be8e5cf4e624ec76
msharipov <msharipovr@gmail.com>
2024-11-27 23:47:23 -0500

    (15_10_1): completed

--- 2015/day10_1/src/main.rs
+++ 2015/day10_1/src/main.rs
@@ -1,7 +1,12 @@
 use std::fs::read_to_string;
 
+use day10_1::generate_next;
+
 fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
-    let input = input.trim();
-    println!("Hello, world!");
+    let mut input = input.trim().to_string();
+    for _ in 0..40 {
+        input = generate_next(&input);
+    }
+    println!("length = {}", input.len());
 }
