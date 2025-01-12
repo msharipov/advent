c85a782441ed8b3c3d517b15bb9eb056ad6fed28
msharipov <msharipovr@gmail.com>
2025-01-11 22:06:39 -0500

    (15_12_1): fix not counting numbers correctly

--- 2015/day12_1/src/lib.rs
+++ 2015/day12_1/src/lib.rs
@@ -1,10 +1,11 @@
 use serde_json::Value;
 
-fn count_numbers(json: &Value) -> i64 {
+fn count_numbers(json: &Value) -> Option<i64> {
     match json {
-        Value::Array(vec) => vec.iter().map(|v| count_numbers(v)).sum(),
-        Value::Object(map) => map.values().map(|v| count_numbers(v)).sum(),
-        _ => 0,
+        Value::Array(vec) => Some(vec.iter().filter_map(|v| count_numbers(v)).sum()),
+        Value::Object(map) => Some(map.values().filter_map(|v| count_numbers(v)).sum()),
+        Value::Number(num) => num.as_i64(),
+        _ => Some(0),
     }
 }
 
@@ -16,6 +17,26 @@ mod tests {
     fn count_numbers_test_1() {
         let string = r#"{}"#;
         let json: Value = serde_json::from_str(string).unwrap();
-        assert_eq!(count_numbers(&json), 0);
+        assert_eq!(count_numbers(&json), Some(0));
+    }
+
+    #[test]
+    fn count_numbers_test_2() {
+        let string = r#"{
+            "name" : "Jimmy",
+            "age" : 30,
+            "numbers" : [
+                1,
+                2,
+                3,
+                4
+            ],
+            "phones" : {
+                "home" : 12345678900,
+                "work" : 98765432100
+            }
+        }"#;
+        let json: Value = serde_json::from_str(string).unwrap();
+        assert_eq!(count_numbers(&json), Some(111111111040));
     }
 }
