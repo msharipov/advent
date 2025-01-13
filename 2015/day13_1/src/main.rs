90682a4535b53d81e6e2f4473948f4b4c4b228d9
msharipov <msharipovr@gmail.com>
2025-01-12 21:29:25 -0500

    (15_13_1): completed

--- 2015/day13_1/src/main.rs
+++ 2015/day13_1/src/main.rs
@@ -1,7 +1,13 @@
+use day13_1::{highest_happiness, parse_preferences};
 use std::fs::read_to_string;
 
 fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
-    let preferences = input.trim().lines();
+    let names = [
+        "Alice", "Bob", "Carol", "David", "Eric", "Frank", "George", "Mallory",
+    ];
+    let preference_lines = input.trim().lines().collect::<Vec<&str>>();
+    let preferences = parse_preferences(&preference_lines).unwrap();
+    let highest = highest_happiness(&names, &preferences).unwrap();
+    println!("The highest possible happiness is: {}", highest);
 }
-
