f8e8b245e74a9445c84e3790da8c5c5267bfcaa4
msharipov <msharipovr@gmail.com>
2025-01-15 21:38:18 -0500

    (15_15_1): compute cookie score

--- 2015/day15_1/src/lib.rs
+++ 2015/day15_1/src/lib.rs
@@ -1,3 +1,5 @@
+use std::cmp::max;
+
 use sscanf::sscanf;
 
 #[derive(Debug, PartialEq)]
@@ -28,6 +30,35 @@ pub fn parse_ingredients(lines: &[&str]) -> Result<Vec<Ingredient>, sscanf::Erro
     lines.iter().map(|line| Ingredient::new(line)).collect()
 }
 
+pub fn cookie_score(recipe: &[(Ingredient, i32)]) -> i32 {
+    let total_capacity = max(
+        recipe
+            .iter()
+            .map(|(ing, spoons)| ing.capacity * spoons)
+            .sum(),
+        0,
+    );
+    let total_durability = max(
+        recipe
+            .iter()
+            .map(|(ing, spoons)| ing.durability * spoons)
+            .sum(),
+        0,
+    );
+    let total_flavor = max(
+        recipe.iter().map(|(ing, spoons)| ing.flavor * spoons).sum(),
+        0,
+    );
+    let total_texture = max(
+        recipe
+            .iter()
+            .map(|(ing, spoons)| ing.texture * spoons)
+            .sum(),
+        0,
+    );
+    total_capacity * total_durability * total_flavor * total_texture
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -48,5 +79,63 @@ mod tests {
                 calories: 8,
             }
         )
+    }
+
+    #[test]
+    fn cookie_score_test_1() {
+        let recipe = [
+            (
+                Ingredient {
+                    name: "Butterscotch".to_owned(),
+                    capacity: -1,
+                    durability: -2,
+                    flavor: 6,
+                    texture: 3,
+                    calories: 8,
+                },
+                20,
+            ),
+            (
+                Ingredient {
+                    name: "Caramel".to_owned(),
+                    capacity: 3,
+                    durability: 1,
+                    flavor: 3,
+                    texture: 1,
+                    calories: 6,
+                },
+                60,
+            ),
+        ];
+        assert_eq!(cookie_score(&recipe), 115_200_000);
+    }
+
+    #[test]
+    fn cookie_score_test_2() {
+        let recipe = [
+            (
+                Ingredient {
+                    name: "Butterscotch".to_owned(),
+                    capacity: -1,
+                    durability: -2,
+                    flavor: 6,
+                    texture: 3,
+                    calories: 8,
+                },
+                50,
+            ),
+            (
+                Ingredient {
+                    name: "Caramel".to_owned(),
+                    capacity: 3,
+                    durability: 1,
+                    flavor: 3,
+                    texture: 1,
+                    calories: 6,
+                },
+                50,
+            ),
+        ];
+        assert_eq!(cookie_score(&recipe), 0);
     }
 }
