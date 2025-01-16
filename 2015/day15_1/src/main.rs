6616f568ced8151edca40ab8a932ea20bb4ef3c2
msharipov <msharipovr@gmail.com>
2025-01-16 00:10:27 -0500

    (15_15_1): completed

--- 2015/day15_1/src/main.rs
+++ 2015/day15_1/src/main.rs
@@ -1,6 +1,10 @@
+use day15_1::{best_score, parse_ingredients};
 use std::fs::read_to_string;
 
 fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
     let input = input.trim().lines().collect::<Vec<_>>();
+    let ingredients = parse_ingredients(&input).unwrap();
+    let best_score = best_score(&ingredients, 100);
+    println!("The best possible score is: {}", best_score);
 }
