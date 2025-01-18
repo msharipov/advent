3899f486636597d500fa91e8fcff0208fe342864
Maxim Sharipov <msharipovr@gmail.com>
2025-01-17 19:30:19 -0500

    (15_16_1): check if Sue matches a filter

--- 2015/day16_1/src/lib.rs
+++ 2015/day16_1/src/lib.rs
@@ -52,6 +52,110 @@ impl Sue {
         }
         Ok(sue)
     }
+
+    pub fn compatible(&self, other: &Sue) -> bool {
+        if let Some(count) = self.children {
+            match other.children {
+                None => return false,
+                Some(other_count) => {
+                    if count != other_count {
+                        return false;
+                    }
+                }
+            }
+        }
+        if let Some(count) = self.cats {
+            match other.cats {
+                None => return false,
+                Some(other_count) => {
+                    if count != other_count {
+                        return false;
+                    }
+                }
+            }
+        }
+        if let Some(count) = self.dogs.akitas {
+            match other.dogs.akitas {
+                None => return false,
+                Some(other_count) => {
+                    if count != other_count {
+                        return false;
+                    }
+                }
+            }
+        }
+        if let Some(count) = self.dogs.pomeranians {
+            match other.dogs.pomeranians {
+                None => return false,
+                Some(other_count) => {
+                    if count != other_count {
+                        return false;
+                    }
+                }
+            }
+        }
+        if let Some(count) = self.dogs.samoyeds {
+            match other.dogs.samoyeds {
+                None => return false,
+                Some(other_count) => {
+                    if count != other_count {
+                        return false;
+                    }
+                }
+            }
+        }
+        if let Some(count) = self.dogs.vizslas {
+            match other.dogs.vizslas {
+                None => return false,
+                Some(other_count) => {
+                    if count != other_count {
+                        return false;
+                    }
+                }
+            }
+        }
+        if let Some(count) = self.goldfish {
+            match other.goldfish {
+                None => return false,
+                Some(other_count) => {
+                    if count != other_count {
+                        return false;
+                    }
+                }
+            }
+        }
+        if let Some(count) = self.cars {
+            match other.cars {
+                None => return false,
+                Some(other_count) => {
+                    if count != other_count {
+                        return false;
+                    }
+                }
+            }
+        }
+        if let Some(count) = self.trees {
+            match other.trees {
+                None => return false,
+                Some(other_count) => {
+                    if count != other_count {
+                        return false;
+                    }
+                }
+            }
+        }
+        if let Some(count) = self.perfumes {
+            match other.perfumes {
+                None => return false,
+                Some(other_count) => {
+                    if count != other_count {
+                        return false;
+                    }
+                }
+            }
+        }
+        true
+    }
 }
 
 #[cfg(test)]
@@ -84,5 +188,35 @@ mod tests {
     fn sue_new_test_3() {
         let sue = Sue::new("Sue 1234");
         assert_eq!(sue, Err("failed to parse the line: Sue 1234".to_owned()));
+    }
+
+    #[test]
+    fn sue_compatible_test_1() {
+        let sue = Sue::new("Sue 1234: perfumes: 2, children: 9, pomeranians: 4").unwrap();
+        let filter = Sue {
+            cats: Some(3),
+            goldfish: Some(6),
+            children: Some(9),
+            perfumes: Some(2),
+            dogs: Dogs {
+                pomeranians: Some(4),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+        assert!(sue.compatible(&filter));
+    }
+
+    #[test]
+    fn sue_compatible_test_2() {
+        let sue = Sue::new("Sue 1234: perfumes: 2, children: 9, pomeranians: 4").unwrap();
+        let filter = Sue {
+            cats: Some(3),
+            goldfish: Some(6),
+            children: Some(9),
+            perfumes: Some(1),
+            ..Default::default()
+        };
+        assert!(!sue.compatible(&filter));
     }
 }
