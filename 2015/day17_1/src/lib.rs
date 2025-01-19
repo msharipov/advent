a5c41667cfce02a9c5198e34a85288145ed1008d
msharipov <msharipovr@gmail.com>
2025-01-19 11:28:43 -0500

    (15_17_1): count possible combinations

--- 2015/day17_1/src/lib.rs
+++ 2015/day17_1/src/lib.rs
@@ -1,5 +1,36 @@
+use itertools::Itertools;
 use std::num::ParseIntError;
 
 pub fn parse_containers(lines: &[&str]) -> Result<Vec<u64>, ParseIntError> {
     lines.iter().map(|line| line.parse::<u64>()).collect()
 }
+
+pub fn count_combinations(containers: &[u64], target: u64) -> u64 {
+    containers
+        .iter()
+        .powerset()
+        .map(|pset| {
+            if pset.iter().copied().sum::<u64>() == target {
+                1
+            } else {
+                0
+            }
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn count_combinations_test_1() {
+        let containers = [1, 2, 3, 2, 4];
+        assert_eq!(count_combinations(&containers, 4), 3);
+    }
+
+    #[test]
+    fn count_combinations_test_2() {
+        let containers = [20, 15, 10, 5, 5];
+        assert_eq!(count_combinations(&containers, 25), 4);
+    }}
