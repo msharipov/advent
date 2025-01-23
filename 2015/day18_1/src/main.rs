2d7491bf6fd16d55b294f38046a901189bc14af8
msharipov <msharipovr@gmail.com>
2025-01-22 22:32:23 -0500

    (15_18_1): completed

--- 2015/day18_1/src/main.rs
+++ 2015/day18_1/src/main.rs
@@ -1,7 +1,13 @@
 use std::fs::read_to_string;
 
+use day18_1::{count_on, next_step, parse_matrix};
+
 fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
     let input = input.trim().lines().collect::<Vec<_>>();
-    println!("Hello, world!");
+    let mut matrix = parse_matrix(&input).unwrap();
+    for _ in 0..100 {
+        matrix = next_step(&matrix);
+    }
+    println!("After 100 steps {} lights are on.", count_on(&matrix));
 }
