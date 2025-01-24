6ab0547147ec4cfcf6cec763e33cda010c022ca6
msharipov <msharipovr@gmail.com>
2025-01-24 11:02:14 -0500

    (15_19_1): completed

--- 2015/day19_1/src/main.rs
+++ 2015/day19_1/src/main.rs
@@ -1,7 +1,13 @@
 use std::fs::read_to_string;
 
+use day19_1::{count_new_sequences, parse_replacements, parse_sequence};
+
 fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
-    let input = input.trim().lines().collect::<Vec<_>>();
+    let mut input = input.trim().lines();
+    let repl = parse_replacements(&mut input).unwrap();
+    let seq = parse_sequence(&mut input).unwrap();
+    let count = count_new_sequences(&seq, &repl);
+    println!("{count} possible sequences");
 }
 
