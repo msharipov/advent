2cf3949430fac6872054e244b6b7701f11572a7c
msharipov <msharipovr@gmail.com>
2025-02-04 23:13:28 -0500

    (15_20_1): implement factoring

--- 2015/day20_1/src/lib.rs
+++ 2015/day20_1/src/lib.rs
@@ -4,6 +4,24 @@ pub fn parse_input(lines: &[&str]) -> Result<i64, ParseIntError> {
     lines[0].parse::<i64>()
 }
 
+pub fn distinct_factors(product: u64) -> Vec<u64> {
+    if product == 1 {
+        return vec![1];
+    }
+    let mut factors = vec![];
+    let mut other_factors = vec![];
+    let root = (product as f64).sqrt().ceil() as u64;
+    for factor in 1..root {
+        if product % factor == 0 {
+            factors.push(factor);
+            other_factors.push(product / factor);
+        }
+    }
+    other_factors.reverse();
+    factors.append(&mut other_factors);
+    factors
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
