66f97f1a0ff35989c44a05de2296164296cda101
msharipov <msharipovr@gmail.com>
2025-02-07 19:33:53 -0500

    (15_20_2): change how gifts are counted

---
+++ 2015/day20_2/src/lib.rs
@@ -0,0 +1,78 @@
+use std::num::ParseIntError;
+
+pub fn parse_input(lines: &[&str]) -> Result<u64, ParseIntError> {
+    lines[0].parse::<u64>()
+}
+
+pub fn distinct_factors(product: u64) -> Vec<u64> {
+    if product == 1 {
+        return vec![1];
+    }
+    let mut factors = vec![];
+    let mut other_factors = vec![];
+    let root = (product as f64).sqrt().floor() as u64;
+    for factor in 1..=root {
+        if product % factor == 0 {
+            factors.push(factor);
+            let other = product / factor;
+            if other != factor {
+                other_factors.push(other);
+            }
+        }
+    }
+    other_factors.reverse();
+    factors.append(&mut other_factors);
+    factors
+}
+
+pub fn count_gifts(house: u64) -> u64 {
+    let mut elves = distinct_factors(house);
+    let cutoff = house / 50;
+    elves.reverse();
+    elves.iter().take_while(|&&e| e >= cutoff).sum::<u64>() * 11
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_input_test_1() {
+        assert_eq!(parse_input(&["1435873"]), Ok(1435873));
+    }
+
+    #[test]
+    fn parse_input_test_2() {
+        assert!(parse_input(&["abracadabra"]).is_err());
+    }
+
+    #[test]
+    fn distinct_factors_test_1() {
+        assert_eq!(distinct_factors(1), vec![1]);
+    }
+
+    #[test]
+    fn distinct_factors_test_2() {
+        assert_eq!(distinct_factors(2), vec![1, 2]);
+    }
+
+    #[test]
+    fn distinct_factors_test_3() {
+        assert_eq!(distinct_factors(12), vec![1, 2, 3, 4, 6, 12]);
+    }
+
+    #[test]
+    fn distinct_factors_test_4() {
+        assert_eq!(distinct_factors(49), vec![1, 7, 49]);
+    }
+
+    #[test]
+    fn count_gifts_test_1() {
+        assert_eq!(count_gifts(1), 11);
+    }
+
+    #[test]
+    fn count_gifts_test_2() {
+        assert_eq!(count_gifts(6), 132);
+    }
+}
---
+++ 2015/day20_2/src/main.rs
@@ -0,0 +1,14 @@
+use std::fs::read_to_string;
+
+use day20_2::{count_gifts, parse_input};
+
+fn main() {
+    let input = read_to_string("input.txt").expect("no input.txt in current directory");
+    let input = input.trim().lines().collect::<Vec<_>>();
+    let gifts = parse_input(&input).unwrap();
+    let mut house = 1;
+    while count_gifts(house) < gifts {
+        house += 1;
+    }
+    println!("House number {}", house);
+}
