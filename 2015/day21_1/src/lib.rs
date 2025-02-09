ba783cbfa93812a1cfbec0e3eb7ea7b7a8bbadd5
msharipov <msharipovr@gmail.com>
2025-02-09 11:52:14 -0500

    (15_21_1): add player struct

--- 2015/day21_1/src/lib.rs
+++ 2015/day21_1/src/lib.rs
@@ -1,15 +1,49 @@
-pub struct Sword {
+#[derive(Default)]
+pub struct Weapon {
     cost: i64,
     damage: i64,
 }
 
+#[derive(Default)]
 pub struct Armor {
     cost: i64,
     armor: i64,
 }
 
+#[derive(Default)]
 pub struct Ring {
     cost: i64,
     damage: i64,
     armor: i64,
+}
+
+#[derive(Default)]
+pub struct Player {
+    pub weapon: Weapon,
+    pub armor: Option<Armor>,
+    pub left_ring: Option<Ring>,
+    pub right_ring: Option<Ring>,
+}
+
+impl Player {
+    pub fn damage(&self) -> i64 {
+        let mut damage = self.weapon.damage;
+        if let Some(ring) = &self.left_ring {
+            damage += ring.damage;
+        }
+        if let Some(ring) = &self.right_ring {
+            damage += ring.damage;
+        }
+        damage
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn player_damage_test_1() {
+        assert_eq!(Player::default().damage(), 0);
+    }
 }
