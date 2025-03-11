a7b8574b3c03c4c9633e8ddeac22a476103322b2
msharipov <msharipovr@gmail.com>
2025-03-11 16:27:37 -0400

    (15_23_2): completed

--- 2015/day23_2/src/main.rs
+++ 2015/day23_2/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs::read_to_string;
 
-use day23_1::{Computer, Instruction, Register};
+use day23_2::{Computer, Instruction, Register};
 
 fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
@@ -10,6 +10,7 @@ fn main() {
         .map(|&line| Instruction::parse(line).unwrap())
         .collect::<Vec<_>>();
     let mut computer = Computer::new(&instructions);
+    computer.set(Register::A, 1);
     while computer.execute_next().is_ok() {}
     println!("Value in b: {}", computer.read(Register::B));
 }
