659662ebb04a6afefdeab751b466dc08b6e72586
msharipov <msharipovr@gmail.com>
2025-03-13 23:30:26 -0400

    (15_24_1): completed

--- 2015/day24_1/src/main.rs
+++ 2015/day24_1/src/main.rs
@@ -1,6 +1,16 @@
-use std::fs::read_to_string;
+use std::{collections::BTreeSet, fs::read_to_string};
+
+use day24_1::{entanglement, parse_weights, partition_into_thirds};
 
 fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
     let input = input.trim().lines().collect::<Vec<_>>();
+    let weights = BTreeSet::from_iter(parse_weights(&input).unwrap());
+    let parts = partition_into_thirds(weights).unwrap();
+    let lowest = parts
+        .iter()
+        .map(|part| entanglement(part))
+        .min()
+        .unwrap();
+    println!("Lowest entanglement: {lowest}");
 }
