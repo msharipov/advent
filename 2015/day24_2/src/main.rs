fcad6f4a45c92c6aa726f0d4f7338883e75405f5
msharipov <msharipovr@gmail.com>
2025-03-14 00:17:26 -0400

    (15_24_2): completed

--- 2015/day24_2/src/main.rs
+++ 2015/day24_2/src/main.rs
@@ -1,15 +1,11 @@
 use std::{collections::BTreeSet, fs::read_to_string};
 
-use day24_2::{entanglement, parse_weights, partition_into_quarters};
+use day24_2::{lowest_entanglement, parse_weights};
 
 fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
     let input = input.trim().lines().collect::<Vec<_>>();
     let weights = BTreeSet::from_iter(parse_weights(&input).unwrap());
-    let lowest = parts
-        .iter()
-        .map(|part| entanglement(part))
-        .min()
-        .unwrap();
+    let lowest = lowest_entanglement(weights).unwrap();
     println!("Lowest entanglement: {lowest}");
 }
