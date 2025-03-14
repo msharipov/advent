6e84b7175ab1a077c382cc82dbb44f7308a88883
msharipov <msharipovr@gmail.com>
2025-03-13 20:26:33 -0400

    (15_24_1): implement partitioning

--- 2015/day24_1/src/lib.rs
+++ 2015/day24_1/src/lib.rs
@@ -1,7 +1,57 @@
-use std::num::ParseIntError;
+use itertools::Itertools;
+use std::{collections::BTreeSet, num::ParseIntError};
+
+type Subset<T> = BTreeSet<T>;
+type Partition<T> = BTreeSet<Subset<T>>;
 
 pub fn parse_weights(lines: &[&str]) -> Result<Vec<u64>, ParseIntError> {
     lines.iter().map(|&line| line.parse()).collect()
+}
+
+#[derive(Debug)]
+pub struct CannotPartition;
+
+pub fn partitions(
+    weights: BTreeSet<u64>,
+    groups: u64,
+) -> Result<BTreeSet<Partition<u64>>, CannotPartition> {
+    if weights.is_empty() {
+        return Err(CannotPartition);
+    }
+    if groups <= 1 {
+        let partition = BTreeSet::from_iter([Subset::from_iter(weights)]);
+        return Ok(BTreeSet::from_iter([partition]));
+    }
+    let sum = weights.iter().sum::<u64>();
+    let group_sum = sum / groups;
+    if sum % groups != 0 || *(weights.iter().max().unwrap()) > group_sum {
+        return Err(CannotPartition);
+    }
+    let mut partitions_found = BTreeSet::new();
+    for subset in weights.iter().cloned().powerset() {
+        if subset.iter().map(|&i| i).sum::<u64>() != group_sum {
+            continue;
+        }
+        let complement = weights
+            .difference(&BTreeSet::from_iter(subset.iter().cloned()))
+            .cloned()
+            .collect();
+        let sub_partitions = partitions(complement, groups - 1);
+        if sub_partitions.is_err() {
+            continue;
+        }
+        let sub_partitions = sub_partitions.unwrap();
+        for mut subpart in sub_partitions {
+            let mut new_partition =
+                BTreeSet::from_iter([Subset::from_iter(subset.iter().cloned())]);
+            new_partition.append(&mut subpart);
+            partitions_found.insert(new_partition);
+        }
+    }
+    if partitions_found.is_empty() {
+        return Err(CannotPartition);
+    }
+    Ok(partitions_found)
 }
 
 #[cfg(test)]
@@ -18,5 +68,16 @@ mod tests {
     fn parse_weights_test_2() {
         let parsed = parse_weights(&["1", "-2", "3", "4"]);
         assert!(parsed.is_err());
+    }
+
+    #[test]
+    fn partitions_test_1() {
+        let weights = BTreeSet::from_iter([1, 2, 3, 4, 5, 6]);
+        let set_1_6 = Subset::from_iter([1, 6]);
+        let set_2_5 = Subset::from_iter([2, 5]);
+        let set_3_4 = Subset::from_iter([3, 4]);
+        let partition = Partition::from_iter([set_1_6, set_2_5, set_3_4]);
+        let correct = BTreeSet::from_iter([partition]);
+        assert_eq!(partitions(weights, 3).unwrap(), correct);
     }
 }
