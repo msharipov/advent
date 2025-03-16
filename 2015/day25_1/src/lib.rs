a6fd2c445568e340c1dfcee4b316c4adbfb8c4e7
msharipov <msharipovr@gmail.com>
2025-03-16 00:05:02 -0400

    (15_25_1): parse input

--- 2015/day25_1/src/lib.rs
+++ 2015/day25_1/src/lib.rs
@@ -1,3 +1,5 @@
+use sscanf::sscanf;
+
 pub fn get_code_index(row: u64, col: u64) -> u64 {
     let header = row + col - 1;
     (header * header - header) / 2 + col
@@ -12,6 +14,10 @@ pub fn compute_code(row: u64, col: u64, seed: u64) -> u64 {
     code
 }
 
+pub fn parse_input(line: &str) -> Result<(u64, u64), sscanf::Error> {
+    sscanf!(line, "To continue, please consult the code grid in the manual.  Enter the code at row {u64}, column {u64}.")
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -24,5 +30,11 @@ mod tests {
     #[test]
     pub fn compute_code_test_1() {
         assert_eq!(compute_code(2, 6, 20151125), 4041754);
+    }
+
+    #[test]
+    pub fn parse_input_test_1() {
+        let line = "To continue, please consult the code grid in the manual.  Enter the code at row 123, column 456.";
+        assert_eq!(parse_input(line).unwrap(), (123, 456));
     }
 }
