1f6a9097a17af81e101e0a291d51e32c1fe59ec4
msharipov <msharipovr@gmail.com>
2025-03-16 01:20:23 -0400

    (15_19_2): completed

--- 2015/day19_2/src/main.rs
+++ 2015/day19_2/src/main.rs
@@ -1,13 +1,12 @@
 use std::fs::read_to_string;
 
-use day19_2::{count_new_sequences, parse_replacements, parse_sequence};
+use day19_2::{count_inversions, parse_replacements, parse_sequence};
 
 fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
     let mut input = input.trim().lines();
     let repl = parse_replacements(&mut input).unwrap();
-    let seq = parse_sequence(&mut input).unwrap();
-    let count = count_new_sequences(&seq, &repl);
-    println!("{count} possible sequences");
+    let mut seq = parse_sequence(&mut input).unwrap();
+    let count = count_inversions(&mut seq, &repl).unwrap();
+    println!("{count} steps needed");
 }
-
