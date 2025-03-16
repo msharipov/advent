c7c01465da25713a9f84c2eefdad8b090c0cfddf
msharipov <msharipovr@gmail.com>
2025-03-16 00:09:42 -0400

    (15_25_1): completed

--- 2015/day25_1/src/main.rs
+++ 2015/day25_1/src/main.rs
@@ -1,3 +1,11 @@
+use std::fs::read_to_string;
+
+use day25_1::{compute_code, parse_input};
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_to_string("input.txt").expect("no input.txt in current directory");
+    let input = input.trim().lines().collect::<Vec<_>>();
+    let (row, col) = parse_input(input[0]).unwrap();
+    let code = compute_code(row, col, 20151125);
+    println!("The code is: {code}");
 }
