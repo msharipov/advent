89b8eaa2a093cd6e61ef5e2e9a2e1862b78f6fb9
msharipov <msharipovr@gmail.com>
2025-03-20 22:25:54 -0400

    (16_1_1): completed

--- 2016/day1_1/src/main.rs
+++ 2016/day1_1/src/main.rs
@@ -1,6 +1,11 @@
 use std::fs::read_to_string;
 
+use day1_1::{parse_moves, taxicab_distance, walk};
+
 fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
     let input = input.trim().lines().collect::<Vec<_>>();
+    let moves = parse_moves(input[0]).unwrap();
+    let distance = taxicab_distance((0, 0), walk(&moves));
+    println!("{distance} blocks away");
 }
