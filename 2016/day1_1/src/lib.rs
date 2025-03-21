8f5b8b5e7c930d2266e0bc24fe916c00dbc21562
msharipov <msharipovr@gmail.com>
2025-03-20 22:23:39 -0400

    (16_1_1): determine final position

--- 2016/day1_1/src/lib.rs
+++ 2016/day1_1/src/lib.rs
@@ -31,6 +31,61 @@ pub fn taxicab_distance(point_a: (i64, i64), point_b: (i64, i64)) -> u64 {
     point_a.0.abs_diff(point_b.0) + point_a.1.abs_diff(point_b.1)
 }
 
+enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+impl Direction {
+    fn cw(&self) -> Self {
+        use Direction::*;
+        match self {
+            North => East,
+            East => South,
+            South => West,
+            West => North,
+        }
+    }
+
+    fn ccw(&self) -> Self {
+        self.cw().cw().cw()
+    }
+}
+
+pub fn walk(moves: &[Move]) -> (i64, i64) {
+    let mut position: (i64, i64) = (0, 0);
+    let mut direction = Direction::North;
+    for m in moves {
+        let distance = match m {
+            Move::R(len) => {
+                direction = direction.cw();
+                *len as i64
+            }
+            Move::L(len) => {
+                direction = direction.ccw();
+                *len as i64
+            }
+        };
+        match direction {
+            Direction::North => {
+                position.1 += distance;
+            }
+            Direction::East => {
+                position.0 += distance;
+            }
+            Direction::South => {
+                position.1 -= distance;
+            }
+            Direction::West => {
+                position.0 -= distance;
+            }
+        }
+    }
+    position
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -61,5 +116,13 @@ mod tests {
     #[test]
     fn taxicab_distance_test_1() {
         assert_eq!(taxicab_distance((-8, 3), (1, -1)), 13);
+    }
+
+    #[test]
+    fn walk_test_1() {
+        let line = "L3, R5, L1, L1, L8";
+        let moves = parse_moves(&line).unwrap();
+        let final_pos = walk(&moves);
+        assert_eq!(final_pos, (4, 4));
     }
 }
