083c8f2048d24fba33b967a641f44f63d91fcb20
msharipov <msharipovr@gmail.com>
2025-03-23 00:40:00 -0400

    (16_3_2): completed

--- 2016/day3_2/src/main.rs
+++ 2016/day3_2/src/main.rs
@@ -1,14 +1,14 @@
 use std::fs::read_to_string;
 
-use day3_2::{parse_sides, valid_sides};
+use day3_2::{parse_all_triangles, valid_sides};
 
 fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
     let input = input.lines().collect::<Vec<_>>();
-    let count = input
+    let count = parse_all_triangles(&input)
+        .unwrap()
         .iter()
-        .map(|line| parse_sides(line).unwrap())
-        .map(|sides| valid_sides(&sides))
+        .map(valid_sides)
         .filter(|&x| x)
         .count();
     println!("{count} valid triangles.")
