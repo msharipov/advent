b46fc22bc7aed9faa3d10526bee93bc3ea0c2116
msharipov <msharipovr@gmail.com>
2025-03-24 17:34:20 -0400

    (16_4_1): compute checksum

--- 2016/day4_1/src/lib.rs
+++ 2016/day4_1/src/lib.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use sscanf::sscanf;
 use std::str::FromStr;
 
@@ -14,6 +15,23 @@ impl FromStr for Room {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let (name, id, checksum) = sscanf!(s, "{:/[a-z\\-]+/}{u32}[{String}]", String)?;
         Ok(Room { name, id, checksum })
+    }
+}
+
+impl Room {
+    fn checksum(&self) -> String {
+        let mut counts = self.name.chars().counts();
+        counts.remove(&'-');
+        counts
+            .iter()
+            .sorted_by(|a, b| match a.1.cmp(b.1) {
+                std::cmp::Ordering::Equal => b.0.cmp(a.0),
+                _ => a.1.cmp(b.1),
+            })
+            .rev()
+            .take(5)
+            .map(|(k, _)| k)
+            .collect()
     }
 }
 
@@ -40,5 +58,15 @@ mod tests {
     fn room_parse_test_2() {
         let line = "asdf-avcv-dsf-[adfsv]";
         assert!(line.parse::<Room>().is_err());
+    }
+
+    #[test]
+    fn room_checksum_test_1() {
+        let room = Room {
+            name: "abr-acad-abra-room".to_owned(),
+            id: 123,
+            checksum: "abcdef".to_owned(),
+        };
+        assert_eq!(room.checksum(), "arboc");
     }
 }
