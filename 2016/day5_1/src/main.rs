ca0eeedb0ad0493836db75d78a4cff3e148996b2
msharipov <msharipovr@gmail.com>
2025-03-25 10:22:35 -0400

    (16_5_1): completed

--- 2016/day5_1/src/main.rs
+++ 2016/day5_1/src/main.rs
@@ -1,6 +1,10 @@
 use std::fs::read_to_string;
 
+use day5_1::compute_code;
+
 fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
     let input = input.trim().lines().collect::<Vec<_>>();
+    let code = compute_code(input[0]);
+    println!("The door code is: {code}");
 }
