b029af6bbdcb68f0744693af7a8ac0ceb9d0e967
msharipov <msharipovr@gmail.com>
2025-03-25 13:36:03 -0400

    (16_7_1): check for ABBA in [...]

--- 2016/day7_1/src/lib.rs
+++ 2016/day7_1/src/lib.rs
@@ -2,13 +2,22 @@ use sscanf::sscanf;
 
 pub fn supports_tls(ipv7: &str) -> Result<bool, sscanf::Error> {
     let mut parts = vec![];
+    let mut in_brackets = vec![];
     let mut current = ipv7;
     while sscanf!(current, "{str}[{str}]{str}").is_ok() {
-        let (first, _, last) = sscanf!(current, "{str}[{str}]{str}")?;
+        let (first, middle, last) = sscanf!(current, "{str}[{str}]{str}")?;
         parts.push(first.to_owned());
+        in_brackets.push(middle.to_owned());
         current = last;
     }
     parts.push(current.to_owned());
+    for part in in_brackets {
+        for window in part.chars().collect::<Vec<_>>().windows(4) {
+            if window[0] == window[3] && window[1] == window[2] && window[0] != window[1] {
+                return Ok(false);
+            }
+        }
+    }
     for part in parts {
         for window in part.chars().collect::<Vec<_>>().windows(4) {
             if window[0] == window[3] && window[1] == window[2] && window[0] != window[1] {
