c40655ec693313c7667fc362812c84e9ceeb83ab
msharipov <msharipovr@gmail.com>
2025-03-25 12:53:52 -0400

    (16_6_1): completed

--- 2016/day6_1/src/main.rs
+++ 2016/day6_1/src/main.rs
@@ -1,6 +1,10 @@
 use std::fs::read_to_string;
 
+use day6_1::decode_message;
+
 fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
     let input = input.trim().lines().collect::<Vec<_>>();
+    let message = decode_message(&input);
+    println!("The message is: {message}");
 }
