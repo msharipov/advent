7babc3c54fca1785a4aa85dff18e2f4ae7aae237
msharipov <msharipovr@gmail.com>
2025-03-25 09:55:44 -0400

    (16_4_2): completed

--- 2016/day4_2/src/main.rs
+++ 2016/day4_2/src/main.rs
@@ -5,11 +5,12 @@ use day4_2::parse_rooms;
 fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
     let input = input.lines().collect::<Vec<_>>();
-    let count = parse_rooms(&input)
+    let found = parse_rooms(&input)
         .unwrap()
-        .iter()
+        .into_iter()
         .filter(|room| room.is_valid())
-        .map(|room| room.sector())
-        .sum::<u32>();
-    println!("The sum of sector IDs is {count}.")
+        .filter(|room| room.decrypt_name().contains("object"));
+    for room in found {
+        println!("{room:?}");
+    }
 }
