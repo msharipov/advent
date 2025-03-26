d0dedc67978309efba724e9dd5881650e2f732f8
msharipov <msharipovr@gmail.com>
2025-03-26 02:13:16 -0400

    (16_8_1): completed

--- 2016/day8_1/src/main.rs
+++ 2016/day8_1/src/main.rs
@@ -1,7 +1,17 @@
 use std::fs::read_to_string;
 
+use day8_1::{Instruction, Screen};
+
 fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
     let input = input.trim().lines().collect::<Vec<_>>();
+    let mut screen = Screen::default();
+    let instructions = input
+        .iter()
+        .map(|line| line.parse::<Instruction>().unwrap());
+    for inst in instructions {
+        screen.apply_instruction(&inst);
+    }
+    let total = screen.total_on();
+    println!("{total} pixels are on.");
 }
-
