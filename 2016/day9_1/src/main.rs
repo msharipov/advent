b0b38fad822498e6f2a7b89a454ef0e1158ead2c
msharipov <msharipovr@gmail.com>
2025-03-28 00:34:55 -0400

    (16_9_1): completed

--- 2016/day9_1/src/main.rs
+++ 2016/day9_1/src/main.rs
@@ -1,3 +1,12 @@
+use std::fs::read_to_string;
+
+use day9_1::decompress;
+
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_to_string("input.txt").expect("no input.txt in current directory");
+    let input = input.trim().lines().collect::<Vec<_>>()[0];
+    let expanded = decompress(input);
+    println!("Expanded text is {} characters long.", expanded.len());
 }
+
