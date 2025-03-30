a5ae6ce8f966728f50221009f5bcfef5067ea8a3
msharipov <msharipovr@gmail.com>
2025-03-29 21:07:38 -0400

    (16_10_1): distinguish other bots and outputs

--- 2016/day10_1/src/lib.rs
+++ 2016/day10_1/src/lib.rs
@@ -4,8 +4,15 @@ use sscanf::sscanf;
 
 // value => initial bot
 type Initial = (u32, u32);
+
+#[derive(Debug, PartialEq)]
+pub enum Destination {
+    Bot(u32),
+    Output(u32),
+}
+
 // number of the bot => (low, high)
-type Transfers = HashMap<u32, (u32, u32)>;
+type Transfers = HashMap<u32, (Destination, Destination)>;
 
 pub fn parse_instructions(lines: &[&str]) -> Result<(Vec<Initial>, Transfers), sscanf::Error> {
     let mut transfers = Transfers::new();
@@ -19,7 +26,28 @@ pub fn parse_instructions(lines: &[&str]) -> Result<(Vec<Initial>, Transfers), s
             line,
             "bot {u32} gives low to bot {u32} and high to bot {u32}"
         ) {
-            transfers.insert(bot, (low, high));
+            transfers.insert(bot, (Destination::Bot(low), Destination::Bot(high)));
+            continue;
+        }
+        if let Ok((bot, low, high)) = sscanf!(
+            line,
+            "bot {u32} gives low to output {u32} and high to bot {u32}"
+        ) {
+            transfers.insert(bot, (Destination::Output(low), Destination::Bot(high)));
+            continue;
+        }
+        if let Ok((bot, low, high)) = sscanf!(
+            line,
+            "bot {u32} gives low to bot {u32} and high to output {u32}"
+        ) {
+            transfers.insert(bot, (Destination::Bot(low), Destination::Output(high)));
+            continue;
+        }
+        if let Ok((bot, low, high)) = sscanf!(
+            line,
+            "bot {u32} gives low to output {u32} and high to output {u32}"
+        ) {
+            transfers.insert(bot, (Destination::Output(low), Destination::Output(high)));
             continue;
         }
         return Err(sscanf::Error::MatchFailed);
@@ -33,14 +61,19 @@ mod tests {
 
     #[test]
     fn parse_instructions_test_1() {
+        use Destination::*;
         let lines = [
             "bot 18 gives low to bot 10 and high to bot 13",
             "value 7 goes to bot 19",
-            "bot 7 gives low to bot 34 and high to bot 16",
-            "bot 9 gives low to bot 50 and high to bot 10",
+            "bot 7 gives low to bot 34 and high to output 16",
+            "bot 9 gives low to output 50 and high to bot 10",
             "value 10 goes to bot 5",
         ];
-        let transfers = Transfers::from_iter([(18, (10, 13)), (7, (34, 16)), (9, (50, 10))]);
+        let transfers = Transfers::from_iter([
+            (18, (Bot(10), Bot(13))),
+            (7, (Bot(34), Output(16))),
+            (9, (Output(50), Bot(10))),
+        ]);
         let initials = vec![(7, 19), (10, 5)];
         assert_eq!(parse_instructions(&lines).unwrap(), (initials, transfers));
     }
