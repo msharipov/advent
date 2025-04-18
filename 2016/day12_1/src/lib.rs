28cddbbd5a735c157bc14141a95016312c6b617c
msharipov <msharipovr@gmail.com>
2025-04-18 00:05:44 -0400

    (16_12_1): parse instruction from str

--- 2016/day12_1/src/lib.rs
+++ 2016/day12_1/src/lib.rs
@@ -34,6 +34,34 @@ pub enum Instruction {
     Jnz(Register, i64),
 }
 
+impl FromStr for Instruction {
+    type Err = sscanf::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if let Ok((x, y)) = sscanf!(s, "cpy {:/a|b|c|d/} {:/a|b|c|d/}", &str, &str) {
+            let x = x.parse::<Register>()?;
+            let y = y.parse::<Register>()?;
+            return Ok(Instruction::Cpy(Operand::Reg(x), y));
+        }
+        if let Ok((x, y)) = sscanf!(s, "cpy {i64} {:/a|b|c|d/}", &str) {
+            let y = y.parse::<Register>()?;
+            return Ok(Instruction::Cpy(Operand::Value(x), y));
+        }
+        if let Ok(reg) = sscanf!(s, "inc {:/a|b|c|d/}", &str) {
+            let reg = reg.parse::<Register>()?;
+            return Ok(Instruction::Inc(reg));
+        }
+        if let Ok(reg) = sscanf!(s, "dec {:/a|b|c|d/}", &str) {
+            let reg = reg.parse::<Register>()?;
+            return Ok(Instruction::Dec(reg));
+        }
+        if let Ok((reg, jump_len)) = sscanf!(s, "jnz {:/a|b|c|d/} {i64}", &str) {
+            let reg = reg.parse::<Register>()?;
+            return Ok(Instruction::Jnz(reg, jump_len));
+        }
+        Err(Self::Err::MatchFailed)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
