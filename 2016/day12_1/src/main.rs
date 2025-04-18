454b1bceb296260ab1f7d6d5a99e31c410d48bc8
msharipov <msharipovr@gmail.com>
2025-04-18 01:36:27 -0400

    (16_12_1): completed

--- 2016/day12_1/src/main.rs
+++ 2016/day12_1/src/main.rs
@@ -1,3 +1,12 @@
+use std::fs::read_to_string;
+
+use day12_1::{Computer, parse_instructions};
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_to_string("input.txt").expect("no input.txt in current directory");
+    let input = input.trim().lines().collect::<Vec<_>>();
+    let instructions = parse_instructions(&input).unwrap();
+    let mut comp = Computer::new(&instructions);
+    comp.run();
+    println!("a = {}", comp.read_reg(day12_1::Register::A));
 }
