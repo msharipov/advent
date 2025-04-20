eff3ca269cc5c9b35d4ef2278a5c3f91595c7a4e
Maxim Sharipov <msharipovr@gmail.com>
2025-04-20 03:14:11 -0400

    (16_13_1): completed

--- 2016/day13_1/src/main.rs
+++ 2016/day13_1/src/main.rs
@@ -1,3 +1,11 @@
+use std::fs::read_to_string;
+
+use day13_1::Maze;
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_to_string("input.txt").expect("no input.txt in current directory");
+    let input = input.trim().parse::<i64>().unwrap();
+    let mut maze = Maze::new(input);
+    let steps = maze.shortest_distance(&(1, 1), &(31, 39)).unwrap();
+    println!("At least {steps} steps are required");
 }
