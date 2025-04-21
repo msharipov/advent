50d0e8558659398c6bd5c3ce4bb6c0a3c0e4118c
msharipov <msharipovr@gmail.com>
2025-04-21 00:22:16 -0400

    (16_13_2): completed

--- 2016/day13_2/src/main.rs
+++ 2016/day13_2/src/main.rs
@@ -6,6 +6,6 @@ fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
     let input = input.trim().parse::<i64>().unwrap();
     let mut maze = Maze::new(input);
-    let steps = maze.shortest_distance(&(1, 1), &(31, 39)).unwrap();
-    println!("At least {steps} steps are required");
+    let cells = maze.accessible_cells(&(1, 1), 50);
+    println!("{cells} cells are accessible");
 }
