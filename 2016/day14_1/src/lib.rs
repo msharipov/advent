6f9dd2f740946b302594b86160fca5ab37b70d97
msharipov <msharipovr@gmail.com>
2025-04-22 21:03:53 -0400

    (16_14_1): check for triplets

---
+++ 2016/day14_1/src/lib.rs
@@ -0,0 +1,28 @@
+fn contains_triplet(hash: &str) -> Option<char> {
+    for triple in hash.as_bytes().windows(3) {
+        if triple[0] == triple[1] && triple[1] == triple[2] {
+            return Some(triple[0] as char);
+        }
+    }
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn contains_triplet_test_1() {
+        assert_eq!(contains_triplet("vdufnvudfniusdfnjnafsdfp"), None);
+    }
+
+    #[test]
+    fn contains_triplet_test_2() {
+        assert_eq!(contains_triplet("vdfvnu3n45334cnasdj23222sad"), Some('2'));
+    }
+
+    #[test]
+    fn contains_triplet_test_3() {
+        assert_eq!(contains_triplet("62m5694mmmk45ml6km4555fdasw"), Some('m'));
+    }
+}
