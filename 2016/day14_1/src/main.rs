1f92f127d7d58050d3c4e98d63ad09ea18a697f4
msharipov <msharipovr@gmail.com>
2025-04-23 13:04:35 -0400

    (16_14_1): completed

--- 2016/day14_1/src/main.rs
+++ 2016/day14_1/src/main.rs
@@ -1,3 +1,10 @@
+use std::fs::read_to_string;
+
+use day14_1::index_of_nth_key;
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_to_string("input.txt").expect("no input.txt in current directory");
+    let input = input.trim().lines().collect::<Vec<_>>();
+    let index = index_of_nth_key(64.try_into().unwrap(), input[0]);
+    println!("The index of 64th key is {index}");
 }
