1bb91655840ebed822dfef140c17d3ed954f7559
msharipov <msharipovr@gmail.com>
2025-04-25 10:03:29 -0400

    (16_17_1): find the shortest path

--- 2016/day17_1/src/lib.rs
+++ 2016/day17_1/src/lib.rs
@@ -1,5 +1,4 @@
-use std::str::FromStr;
-
+#[derive(Debug, PartialEq, Clone)]
 pub enum Move {
     Up,
     Down,
@@ -42,6 +41,45 @@ impl OpenDoors {
     }
 }
 
+type Path = (u8, u8, Vec<Move>);
+
+pub fn shortest_path(salt: &str) -> Option<Vec<Move>> {
+    let mut horizon: Vec<Path> = vec![(0, 0, vec![])];
+    loop {
+        if horizon.is_empty() {
+            return None;
+        }
+        let mut new_horizon = vec![];
+        for (row, col, moves) in horizon {
+            if row == 3 && col == 3 {
+                return Some(moves);
+            }
+            let open_doors = OpenDoors::new(salt, &moves);
+            if row > 0 && open_doors.up {
+                let mut new_moves = moves.clone();
+                new_moves.push(Move::Up);
+                new_horizon.push((row - 1, col, new_moves));
+            }
+            if row < 3 && open_doors.down {
+                let mut new_moves = moves.clone();
+                new_moves.push(Move::Down);
+                new_horizon.push((row + 1, col, new_moves));
+            }
+            if col > 0 && open_doors.left {
+                let mut new_moves = moves.clone();
+                new_moves.push(Move::Left);
+                new_horizon.push((row, col - 1, new_moves));
+            }
+            if col < 3 && open_doors.right {
+                let mut new_moves = moves.clone();
+                new_moves.push(Move::Right);
+                new_horizon.push((row, col + 1, new_moves));
+            }
+        }
+        horizon = new_horizon;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -57,5 +95,21 @@ mod tests {
                 right: false
             }
         )
+    }
+
+    #[test]
+    fn shortest_path_test_1() {
+        assert_eq!(
+            moves_to_string(&shortest_path("ihgpwlah").unwrap()),
+            "DDRRRD"
+        );
+    }
+
+    #[test]
+    fn shortest_path_test_2() {
+        assert_eq!(
+            moves_to_string(&shortest_path("kglvqrro").unwrap()),
+            "DDUDRLRRUDRD"
+        );
     }
 }
