a1e9e2640cd1f69b91e302c4dcbe1a24eaa97d1b
msharipov <msharipovr@gmail.com>
2025-04-25 00:46:45 -0400

    (16_16_1): completed

--- 2016/day16_1/src/main.rs
+++ 2016/day16_1/src/main.rs
@@ -1,3 +1,11 @@
+use std::fs::read_to_string;
+
+use day16_1::{checksum, generate_data};
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_to_string("input.txt").expect("no input.txt in current directory");
+    let input = input.trim().lines().collect::<Vec<_>>();
+    let data = generate_data(input[0], 272);
+    println!("The checksum is {}", checksum(&data));
 }
+
