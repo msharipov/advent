023af22521b20594e5837fde74a9c0231435924f
msharipov <msharipovr@gmail.com>
2025-04-25 10:20:00 -0400

    (16_17_2): completed

--- 2016/day17_2/src/main.rs
+++ 2016/day17_2/src/main.rs
@@ -1,10 +1,9 @@
 use std::fs::read_to_string;
 
-use day17_2::{moves_to_string, shortest_path};
+use day17_2::longest_path;
 
 fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
     let input = input.trim().lines().collect::<Vec<_>>()[0];
-    let path = shortest_path(input).unwrap();
-    println!("The shortest path is: {}", moves_to_string(&path));
+    println!("The length of the longest path is: {}", longest_path(input));
 }
