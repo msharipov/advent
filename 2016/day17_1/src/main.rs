0d46c1503fead26ddf0245cf9cc415ec34754fba
msharipov <msharipovr@gmail.com>
2025-04-25 10:07:25 -0400

    (16_17_1): completed

--- 2016/day17_1/src/main.rs
+++ 2016/day17_1/src/main.rs
@@ -1,3 +1,10 @@
+use std::fs::read_to_string;
+
+use day17_1::{moves_to_string, shortest_path};
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_to_string("input.txt").expect("no input.txt in current directory");
+    let input = input.trim().lines().collect::<Vec<_>>()[0];
+    let path = shortest_path(input).unwrap();
+    println!("The shortest path is: {}", moves_to_string(&path));
 }
