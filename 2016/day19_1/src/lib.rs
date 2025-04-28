168166388725ea32fc1334b908b00998b547aa83
msharipov <msharipovr@gmail.com>
2025-04-28 13:04:40 -0400

    (16_19_1): get index of next target

---
+++ 2016/day19_1/src/lib.rs
@@ -0,0 +1,33 @@
+fn get_target(skipped: &[bool], thief_index: usize) -> Option<usize> {
+    if !matches!(skipped.get(thief_index), Some(false)) {
+        return None;
+    }
+    let mut current_index = thief_index;
+    let len = skipped.len();
+    loop {
+        current_index = (current_index + 1) % len;
+        if !skipped[current_index] {
+            return Some(current_index);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_target_test_1() {
+        assert_eq!(get_target(&[true, true, false, false, true], 3), Some(2));
+    }
+
+    #[test]
+    fn get_target_test_2() {
+        assert_eq!(get_target(&[true, true, true, false, true], 3), Some(3));
+    }
+
+    #[test]
+    fn get_target_test_3() {
+        assert_eq!(get_target(&[true, true, true, true, true], 3), None);
+    }
+}
