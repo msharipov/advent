14a8fc73b76bacbe5e11fa8779c6760e6fa134d0
msharipov <msharipovr@gmail.com>
2025-04-28 13:21:43 -0400

    (16_19_1): determine the last elf

--- 2016/day19_1/src/lib.rs
+++ 2016/day19_1/src/lib.rs
@@ -1,3 +1,5 @@
+use std::num::NonZero;
+
 fn next_unskipped(skipped: &[bool], thief_index: usize) -> Option<usize> {
     if !matches!(skipped.get(thief_index), Some(false)) {
         return None;
@@ -9,6 +11,21 @@ fn next_unskipped(skipped: &[bool], thief_index: usize) -> Option<usize> {
         if !skipped[current_index] {
             return Some(current_index);
         }
+    }
+}
+
+pub fn last_elf_index(count: NonZero<usize>) -> usize {
+    let mut skipped = vec![false; count.into()];
+    let mut current_thief = 0;
+    loop {
+        let target = next_unskipped(&skipped, current_thief)
+            .expect("there should always be at least one unskipped elf");
+        if target == current_thief {
+            return current_thief;
+        }
+        skipped[target] = true;
+        current_thief = next_unskipped(&skipped, current_thief)
+            .expect("there should always be at least one unskipped elf");
     }
 }
 
@@ -32,5 +49,15 @@ mod tests {
     #[test]
     fn get_target_test_3() {
         assert_eq!(next_unskipped(&[true, true, true, true, true], 3), None);
+    }
+
+    #[test]
+    fn last_elf_index_test_1() {
+        assert_eq!(last_elf_index(1.try_into().unwrap()), 0);
+    }
+
+    #[test]
+    fn last_elf_index_test_2() {
+        assert_eq!(last_elf_index(5.try_into().unwrap()), 2);
     }
 }
