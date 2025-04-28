f5371e19090841e811b2a67a23361fa1dddfeffa
msharipov <msharipovr@gmail.com>
2025-04-28 00:41:42 -0400

    (16_18_1): completed

--- 2016/day18_1/src/main.rs
+++ 2016/day18_1/src/main.rs
@@ -1,3 +1,12 @@
+use std::fs::read_to_string;
+
+use day18_1::{count_safe, generate_floor, parse_row};
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_to_string("input.txt").expect("no input.txt in current directory");
+    let input = input.trim().lines().collect::<Vec<_>>()[0];
+    let floor = generate_floor(&parse_row(input).unwrap(), 40);
+    let count = count_safe(&floor);
+    println!("{count} safe tiles");
 }
+
