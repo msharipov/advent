b0fe40e942aaf6b96021d0eeb1ae9a1e25a53c66
msharipov <msharipovr@gmail.com>
2025-04-29 14:15:41 -0400

    (16_19_2): completed

--- 2016/day19_2/src/main.rs
+++ 2016/day19_2/src/main.rs
@@ -6,8 +6,5 @@ fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
     let input = input.trim().lines().collect::<Vec<_>>()[0];
     let count = input.parse::<usize>().unwrap();
-    println!(
-        "The last elf is #{}",
-        last_elf(count.try_into().unwrap()) + 1
-    );
+    println!("The last elf is #{}", last_elf(count.try_into().unwrap()));
 }
