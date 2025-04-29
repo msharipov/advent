958c828554dff9e2022214eaf5cbc65ff12a365e
msharipov <msharipovr@gmail.com>
2025-04-29 15:49:12 -0400

    (16_20_2): completed

--- 2016/day20_2/src/main.rs
+++ 2016/day20_2/src/main.rs
@@ -1,14 +1,10 @@
 use std::fs::read_to_string;
 
-use day20_1::{lowest_allowed_ip, parse_ranges};
+use day20_2::{count_allowed_ips, parse_ranges};
 
 fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
     let input = input.trim().lines().collect::<Vec<_>>();
     let ranges = parse_ranges(&input).unwrap();
-    println!(
-        "The lowest allowed IP is {}",
-        lowest_allowed_ip(&ranges).unwrap()
-    );
+    println!("{} IPs are allowed", count_allowed_ips(&ranges));
 }
-
