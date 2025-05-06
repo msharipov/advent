ecb612b3458528f2457f7874d69759f719abbeda
msharipov <msharipovr@gmail.com>
2025-05-06 11:53:25 -0400

    (16_23_2): completed

---
+++ 2016/day23_2/src/lib.rs
@@ -0,0 +1,409 @@
+use sscanf::sscanf;
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum Register {
+    A,
+    B,
+    C,
+    D,
+}
+
+impl FromStr for Register {
+    type Err = sscanf::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "a" => Ok(Register::A),
+            "b" => Ok(Register::B),
+            "c" => Ok(Register::C),
+            "d" => Ok(Register::D),
+            _ => Err(sscanf::Error::MatchFailed),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum Operand {
+    Reg(Register),
+    Value(i64),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Instruction {
+    Cpy(Operand, Operand),
+    Inc(Register),
+    Dec(Register),
+    Jnz(Operand, Operand),
+    Tgl(Register),
+}
+
+impl FromStr for Instruction {
+    type Err = sscanf::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if let Ok((x, y)) = sscanf!(s, "cpy {:/a|b|c|d/} {:/a|b|c|d/}", &str, &str) {
+            let x = x.parse::<Register>()?;
+            let y = y.parse::<Register>()?;
+            return Ok(Instruction::Cpy(Operand::Reg(x), Operand::Reg(y)));
+        }
+        if let Ok((x, y)) = sscanf!(s, "cpy {i64} {:/a|b|c|d/}", &str) {
+            let y = y.parse::<Register>()?;
+            return Ok(Instruction::Cpy(Operand::Value(x), Operand::Reg(y)));
+        }
+        if let Ok(reg) = sscanf!(s, "inc {:/a|b|c|d/}", &str) {
+            let reg = reg.parse::<Register>()?;
+            return Ok(Instruction::Inc(reg));
+        }
+        if let Ok(reg) = sscanf!(s, "dec {:/a|b|c|d/}", &str) {
+            let reg = reg.parse::<Register>()?;
+            return Ok(Instruction::Dec(reg));
+        }
+        if let Ok((cond, jump_len)) = sscanf!(s, "jnz {&str:/a|b|c|d/} {&str:/a|b|c|d/}") {
+            let cond = cond.parse::<Register>()?;
+            let jump_len = jump_len.parse::<Register>()?;
+            return Ok(Instruction::Jnz(Operand::Reg(cond), Operand::Reg(jump_len)));
+        }
+        if let Ok((cond, jump_len)) = sscanf!(s, "jnz {i64} {&str:/a|b|c|d/}") {
+            let jump_len = jump_len.parse::<Register>()?;
+            return Ok(Instruction::Jnz(
+                Operand::Value(cond),
+                Operand::Reg(jump_len),
+            ));
+        }
+        if let Ok((reg, jump_len)) = sscanf!(s, "jnz {:/a|b|c|d/} {i64}", &str) {
+            let reg = reg.parse::<Register>()?;
+            return Ok(Instruction::Jnz(
+                Operand::Reg(reg),
+                Operand::Value(jump_len),
+            ));
+        }
+        if let Ok((cond, jump_len)) = sscanf!(s, "jnz {i64} {i64}") {
+            return Ok(Instruction::Jnz(
+                Operand::Value(cond),
+                Operand::Value(jump_len),
+            ));
+        }
+        if let Ok(reg) = sscanf!(s, "tgl {&str:/a|b|c|d/}") {
+            let reg = reg.parse::<Register>()?;
+            return Ok(Instruction::Tgl(reg));
+        }
+        Err(Self::Err::MatchFailed)
+    }
+}
+
+pub fn parse_instructions(lines: &[&str]) -> Result<Vec<Instruction>, sscanf::Error> {
+    lines
+        .iter()
+        .map(|&line| line.parse::<Instruction>())
+        .collect()
+}
+
+pub struct Halt;
+
+#[derive(Debug, PartialEq)]
+pub struct Computer {
+    iar: i64,
+    instructions: Vec<Instruction>,
+    ra: i64,
+    rb: i64,
+    rc: i64,
+    rd: i64,
+}
+
+impl Computer {
+    pub fn new(instructions: &[Instruction]) -> Self {
+        Computer {
+            iar: 0,
+            ra: 0,
+            rb: 0,
+            rc: 0,
+            rd: 0,
+            instructions: instructions.to_vec(),
+        }
+    }
+
+    pub fn read_reg(&self, reg: Register) -> i64 {
+        match reg {
+            Register::A => self.ra,
+            Register::B => self.rb,
+            Register::C => self.rc,
+            Register::D => self.rd,
+        }
+    }
+
+    pub fn set_reg(&mut self, reg: Register, val: i64) {
+        let reg = match reg {
+            Register::A => &mut self.ra,
+            Register::B => &mut self.rb,
+            Register::C => &mut self.rc,
+            Register::D => &mut self.rd,
+        };
+        *reg = val;
+    }
+
+    fn cpy(&mut self, op_from: Operand, op_to: Operand) {
+        if let Operand::Reg(op_to) = op_to {
+            match op_from {
+                Operand::Reg(reg) => {
+                    self.set_reg(op_to, self.read_reg(reg));
+                }
+                Operand::Value(val) => {
+                    self.set_reg(op_to, val);
+                }
+            }
+        }
+        self.iar += 1;
+    }
+
+    fn inc(&mut self, reg: Register) {
+        self.set_reg(reg, self.read_reg(reg) + 1);
+        self.iar += 1;
+    }
+
+    fn dec(&mut self, reg: Register) {
+        self.set_reg(reg, self.read_reg(reg) - 1);
+        self.iar += 1;
+    }
+
+    fn jnz(&mut self, cond: Operand, jump_len: Operand) {
+        let val = match cond {
+            Operand::Value(val) => val,
+            Operand::Reg(reg) => self.read_reg(reg),
+        };
+        if val != 0 {
+            self.iar += match jump_len {
+                Operand::Value(val) => val,
+                Operand::Reg(r) => self.read_reg(r),
+            };
+        } else {
+            self.iar += 1;
+        }
+    }
+
+    fn tgl(&mut self, reg: Register) {
+        let offset = self.read_reg(reg);
+        let index = self.iar + offset;
+        if index < 0 || index >= self.instructions.len() as i64 {
+            self.iar += 1;
+            return;
+        }
+        let index = index as usize;
+        self.instructions[index] = match self.instructions[index] {
+            Instruction::Cpy(op1, op2) => Instruction::Jnz(op1, op2),
+            Instruction::Jnz(op1, op2) => Instruction::Cpy(op1, op2),
+            Instruction::Dec(op) => Instruction::Inc(op),
+            Instruction::Inc(op) => Instruction::Dec(op),
+            Instruction::Tgl(op) => Instruction::Inc(op),
+        };
+        self.iar += 1;
+    }
+
+    pub fn next_step(&mut self) -> Result<(), Halt> {
+        use Instruction::*;
+        if self.iar < 0 {
+            return Err(Halt);
+        }
+        match self.instructions.get(self.iar as usize) {
+            None => Err(Halt),
+            Some(inst) => {
+                match inst {
+                    Cpy(op_from, op_to) => {
+                        self.cpy(*op_from, *op_to);
+                    }
+                    Inc(reg) => {
+                        self.inc(*reg);
+                    }
+                    Dec(reg) => {
+                        self.dec(*reg);
+                    }
+                    Jnz(cond, jump_len) => {
+                        self.jnz(*cond, *jump_len);
+                    }
+                    Tgl(reg) => {
+                        self.tgl(*reg);
+                    }
+                }
+                Ok(())
+            }
+        }
+    }
+
+    pub fn run(&mut self) {
+        while self.next_step().is_ok() {}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn register_from_str_test_1() {
+        assert_eq!("a".parse::<Register>().unwrap(), Register::A);
+    }
+
+    #[test]
+    fn register_from_str_test_2() {
+        assert!("p".parse::<Register>().is_err());
+    }
+
+    #[test]
+    fn parse_instructions_test_1() {
+        use Instruction::*;
+        let lines = [
+            "cpy b a",
+            "cpy 12 c",
+            "dec d",
+            "inc c",
+            "jnz a -19",
+            "jnz 5 12",
+            "tgl c",
+        ];
+        let correct = vec![
+            Cpy(Operand::Reg(Register::B), Operand::Reg(Register::A)),
+            Cpy(Operand::Value(12), Operand::Reg(Register::C)),
+            Dec(Register::D),
+            Inc(Register::C),
+            Jnz(Operand::Reg(Register::A), Operand::Value(-19)),
+            Jnz(Operand::Value(5), Operand::Value(12)),
+            Tgl(Register::C),
+        ];
+        assert_eq!(parse_instructions(&lines).unwrap(), correct);
+    }
+
+    #[test]
+    fn parse_instructions_test_2() {
+        let lines = [
+            "cpy a 13",
+            "cpy 12 c",
+            "dec d",
+            "inc c",
+            "jnz a -19",
+            "jnz 5 12",
+        ];
+        assert!(parse_instructions(&lines).is_err());
+    }
+
+    #[test]
+    fn cpy_test_1() {
+        let mut comp = Computer::new(&[]);
+        comp.set_reg(Register::A, 18);
+        comp.set_reg(Register::B, -12);
+        assert_eq!(comp.read_reg(Register::A), 18);
+        comp.cpy(Operand::Reg(Register::B), Operand::Reg(Register::A));
+        assert_eq!(comp.read_reg(Register::A), -12);
+    }
+
+    #[test]
+    fn cpy_test_2() {
+        let mut comp = Computer::new(&[]);
+        comp.set_reg(Register::A, 18);
+        assert_eq!(comp.read_reg(Register::A), 18);
+        comp.cpy(Operand::Value(45), Operand::Reg(Register::A));
+        assert_eq!(comp.read_reg(Register::A), 45);
+    }
+
+    #[test]
+    fn inc_test_1() {
+        let mut comp = Computer::new(&[]);
+        comp.set_reg(Register::B, 15);
+        comp.inc(Register::B);
+        comp.inc(Register::B);
+        comp.inc(Register::B);
+        comp.inc(Register::B);
+        assert_eq!(comp.read_reg(Register::B), 19);
+    }
+
+    #[test]
+    fn dec_test_1() {
+        let mut comp = Computer::new(&[]);
+        comp.set_reg(Register::B, 15);
+        comp.dec(Register::B);
+        comp.dec(Register::B);
+        comp.dec(Register::B);
+        comp.dec(Register::B);
+        assert_eq!(comp.read_reg(Register::B), 11);
+    }
+
+    #[test]
+    fn jnz_test_1() {
+        let mut comp = Computer::new(&[]);
+        assert_eq!(comp.iar, 0);
+        comp.set_reg(Register::B, 5);
+        comp.jnz(Operand::Reg(Register::B), Operand::Value(14));
+        assert_eq!(comp.iar, 14);
+    }
+
+    #[test]
+    fn jnz_test_2() {
+        let mut comp = Computer::new(&[]);
+        assert_eq!(comp.iar, 0);
+        comp.jnz(Operand::Reg(Register::B), Operand::Value(14));
+        assert_eq!(comp.iar, 1);
+    }
+
+    #[test]
+    fn jnz_test_3() {
+        let mut comp = Computer::new(&[]);
+        assert_eq!(comp.iar, 0);
+        comp.jnz(Operand::Value(11), Operand::Value(14));
+        assert_eq!(comp.iar, 14);
+    }
+
+    #[test]
+    fn tgl_test_1() {
+        let mut comp = Computer::new(&[
+            Instruction::Tgl(Register::A),
+            Instruction::Tgl(Register::A),
+            Instruction::Tgl(Register::A),
+            Instruction::Tgl(Register::A),
+            Instruction::Tgl(Register::B),
+            Instruction::Inc(Register::C),
+            Instruction::Dec(Register::C),
+            Instruction::Cpy(Operand::Value(0), Operand::Reg(Register::B)),
+            Instruction::Jnz(Operand::Reg(Register::D), Operand::Reg(Register::B)),
+        ]);
+        comp.set_reg(Register::A, 5);
+        comp.set_reg(Register::B, -4);
+        comp.run();
+        assert_eq!(
+            comp.instructions,
+            [
+                Instruction::Inc(Register::A),
+                Instruction::Tgl(Register::A),
+                Instruction::Tgl(Register::A),
+                Instruction::Tgl(Register::A),
+                Instruction::Tgl(Register::B),
+                Instruction::Dec(Register::C),
+                Instruction::Inc(Register::C),
+                Instruction::Jnz(Operand::Value(0), Operand::Reg(Register::B)),
+                Instruction::Cpy(Operand::Reg(Register::D), Operand::Reg(Register::B)),
+            ]
+        )
+    }
+
+    #[test]
+    fn run_test_1() {
+        let instructions = [
+            "cpy 10 a", "inc b", "inc c", "inc c", "inc d", "inc d", "inc d", "dec a", "jnz a -7",
+        ];
+        let instructions = parse_instructions(&instructions).unwrap();
+        let mut comp = Computer::new(&instructions);
+        comp.run();
+        assert_eq!(comp.read_reg(Register::A), 0);
+        assert_eq!(comp.read_reg(Register::B), 10);
+        assert_eq!(comp.read_reg(Register::C), 20);
+        assert_eq!(comp.read_reg(Register::D), 30);
+    }
+
+    #[test]
+    fn run_test_2() {
+        let instructions = [
+            "cpy 2 a", "tgl a", "tgl a", "tgl a", "cpy 1 a", "dec a", "dec a",
+        ];
+        let instructions = parse_instructions(&instructions).unwrap();
+        let mut comp = Computer::new(&instructions);
+        comp.set_reg(Register::A, 7);
+        comp.run();
+        assert_eq!(comp.read_reg(Register::A), 3);
+    }
+}
---
+++ 2016/day23_2/src/main.rs
@@ -0,0 +1,13 @@
+use std::fs::read_to_string;
+
+use day23_1::{Computer, Register, parse_instructions};
+
+fn main() {
+    let input = read_to_string("input.txt").expect("no input.txt in current directory");
+    let input = input.trim().lines().collect::<Vec<_>>();
+    let instructions = parse_instructions(&input).unwrap();
+    let mut comp = Computer::new(&instructions);
+    comp.set_reg(Register::A, 12);
+    comp.run();
+    println!("{}", comp.read_reg(Register::A));
+}
