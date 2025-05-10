8088e02ccb94bc6a8f13702ea3ae5affec6c5d1f
msharipov <msharipovr@gmail.com>
2025-05-09 22:51:47 -0400

    (16_24_1): construct a map from tiles

--- 2016/day24_1/src/lib.rs
+++ 2016/day24_1/src/lib.rs
@@ -1,6 +1,7 @@
+use ndarray::Array2;
 use thiserror::Error;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Tile {
     Floor,
     Wall,
@@ -26,8 +27,27 @@ impl TryFrom<char> for Tile {
     }
 }
 
+pub fn parse_map(lines: &[&str]) -> Result<Array2<Tile>, ParseTileError> {
+    if lines.is_empty() {
+        return Ok(Array2::from_elem((0, 0), Tile::Floor));
+    }
+    let width = lines[0].len();
+    let mut map = vec![];
+    for line in lines {
+        if line.len() != width {
+            panic!("uneven line width")
+        }
+        for c in line.chars() {
+            map.push(c.try_into()?);
+        }
+    }
+    Ok(Array2::from_shape_vec((lines.len(), width), map).unwrap())
+}
+
 #[cfg(test)]
 mod tests {
+    use ndarray::array;
+
     use super::*;
 
     #[test]
@@ -41,5 +61,17 @@ mod tests {
     fn tile_try_from_test_2() {
         let result = Tile::try_from('y');
         assert_eq!(result, Err(ParseTileError { c: 'y' }));
+    }
+
+    #[test]
+    fn parse_map_test_1() {
+        use Tile::*;
+        let lines = ["#...2", "..3##", "#.#.#"];
+        let correct = array![
+            [Wall, Floor, Floor, Floor, Marker(2)],
+            [Floor, Floor, Marker(3), Wall, Wall],
+            [Wall, Floor, Wall, Floor, Wall]
+        ];
+        assert_eq!(parse_map(&lines), Ok(correct));
     }
 }
