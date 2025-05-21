332f95e1b618683bbe0d495cdc1e6c2ae9c6f3ea
msharipov <msharipovr@gmail.com>
2025-05-21 11:41:36 -0400

    (16_24_1): completed

--- 2016/day24_1/src/main.rs
+++ 2016/day24_1/src/main.rs
@@ -1,3 +1,11 @@
+use std::fs::read_to_string;
+
+use day24_1::Map;
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_to_string("input.txt").expect("no input.txt in current directory");
+    let input = input.trim().lines().collect::<Vec<_>>();
+    let map = Map::parse_map(&input).unwrap();
+    let shortest = map.shortest_trip_length(0).unwrap();
+    println!("Shortest trip length is: {shortest}");
 }
