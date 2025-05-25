38d16f3a82b018c0e13573656212636cf2cb2f53
msharipov <msharipovr@gmail.com>
2025-05-25 11:12:41 -0400

    (16_25_1): completed

--- 2016/day25_1/src/main.rs
+++ 2016/day25_1/src/main.rs
@@ -1,12 +1,11 @@
 use std::fs::read_to_string;
 
-use day25_1::{Computer, Register, parse_instructions};
+use day25_1::{first_valid_a_value, parse_instructions};
 
 fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
     let input = input.trim().lines().collect::<Vec<_>>();
     let instructions = parse_instructions(&input).unwrap();
-    let mut comp = Computer::new(&instructions);
-    comp.run();
-    println!("a = {}", comp.read_reg(Register::A));
+    let lowest = first_valid_a_value(&instructions).unwrap();
+    println!("a = {lowest}");
 }
