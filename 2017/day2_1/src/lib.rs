76a516e2e5277a0f5e891f4f1daa0d0e6dd6e91b
Maxim Sharipov <msharipovr@gmail.com>
2025-06-01 08:59:06 -0400

    (17_2_1): parse lines into numbers

--- 2017/day2_1/src/lib.rs
+++ 2017/day2_1/src/lib.rs
@@ -1,5 +1,19 @@
+use std::num::ParseIntError;
+
 fn max_minus_min(list: &[i64]) -> Option<i64> {
     Some(list.iter().max()? - list.iter().min()?)
+}
+
+fn parse_lines(lines: &[&str]) -> Result<Vec<Vec<i64>>, ParseIntError> {
+    let mut vecs = vec![];
+    for line in lines {
+        vecs.push(
+            line.split(' ')
+                .map(|num| num.parse())
+                .collect::<Result<Vec<_>, ParseIntError>>()?,
+        );
+    }
+    Ok(vecs)
 }
 
 #[cfg(test)]
@@ -19,5 +33,18 @@ mod tests {
     #[test]
     fn max_minus_min_test_3() {
         assert_eq!(max_minus_min(&[]), None)
+    }
+
+    #[test]
+    fn parse_lines_test_1() {
+        let lines = ["12 34 56 78", "1 2 3 4567 890"];
+        let correct = vec![vec![12, 34, 56, 78], vec![1, 2, 3, 4567, 890]];
+        assert_eq!(parse_lines(&lines), Ok(correct));
+    }
+
+    #[test]
+    fn parse_lines_test_2() {
+        let lines = ["12 34 56 78", "1 2 c 4567 890"];
+        assert!(parse_lines(&lines).is_err());
     }
 }
