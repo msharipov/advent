b89c1064f1354406699a32092fa90f0e2ccd0952
msharipov <msharipovr@gmail.com>
2025-06-03 11:44:42 -0400

    (17_2_2): find the divisble pair

---
+++ 2017/day2_2/src/lib.rs
@@ -0,0 +1,97 @@
+use itertools::Itertools;
+use std::num::ParseIntError;
+
+fn max_minus_min(list: &[i64]) -> Option<i64> {
+    Some(list.iter().max()? - list.iter().min()?)
+}
+
+pub fn parse_lines(lines: &[&str]) -> Result<Vec<Vec<i64>>, ParseIntError> {
+    let mut vecs = vec![];
+    for line in lines {
+        vecs.push(
+            line.split([' ', '\t'])
+                .map(|num| num.parse())
+                .collect::<Result<Vec<_>, ParseIntError>>()?,
+        );
+    }
+    Ok(vecs)
+}
+
+pub fn compute_checksum(numbers: &[Vec<i64>]) -> Option<i64> {
+    numbers.iter().map(|vec| max_minus_min(vec)).sum()
+}
+
+fn evenly_divisible_pair(list: &[i64]) -> Option<(i64, i64)> {
+    let divisible_pair = list
+        .iter()
+        .permutations(2)
+        .find(|pair| pair[0] % pair[1] == 0)?;
+    Some((*divisible_pair[0], *divisible_pair[1]))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn max_minus_min_test_1() {
+        assert_eq!(max_minus_min(&[2, -5, 12, 9, -3, 0, 14]), Some(19))
+    }
+
+    #[test]
+    fn max_minus_min_test_2() {
+        assert_eq!(max_minus_min(&[11]), Some(0))
+    }
+
+    #[test]
+    fn max_minus_min_test_3() {
+        assert_eq!(max_minus_min(&[]), None)
+    }
+
+    #[test]
+    fn parse_lines_test_1() {
+        let lines = ["12 34 56 78", "1 2 3\t4567 890"];
+        let correct = vec![vec![12, 34, 56, 78], vec![1, 2, 3, 4567, 890]];
+        assert_eq!(parse_lines(&lines), Ok(correct));
+    }
+
+    #[test]
+    fn parse_lines_test_2() {
+        let lines = ["12 34 56 78", "1 2 c 4567 890"];
+        assert!(parse_lines(&lines).is_err());
+    }
+
+    #[test]
+    fn compute_checksum_test_1() {
+        let numbers = [
+            vec![-43, 56, -123, 74, 153, 90, 25],
+            vec![56, 14, -45, 100, 40, 431, -59],
+            vec![504],
+        ];
+        assert_eq!(compute_checksum(&numbers), Some(276 + 490));
+    }
+
+    #[test]
+    fn compute_checksum_test_2() {
+        let numbers = [
+            vec![-43, 56, -123, 74, 153, 90, 25],
+            vec![],
+            vec![56, 14, -45, 100, 40, 431, -59],
+            vec![504],
+        ];
+        assert_eq!(compute_checksum(&numbers), None);
+    }
+
+    #[test]
+    fn evenly_divisible_pair_test_1() {
+        assert_eq!(
+            evenly_divisible_pair(&[13, 22, 10, 15, 9, 17, 27]),
+            Some((27, 9))
+        );
+    }
+
+    #[test]
+    fn evenly_divisible_pair_test_2() {
+        assert_eq!(evenly_divisible_pair(&[13, 22, 10, 15, 4, 17, 27]), None);
+    }
+}
---
+++ 2017/day2_2/src/main.rs
@@ -0,0 +1,11 @@
+use std::fs::read_to_string;
+
+use day2_2::{compute_checksum, parse_lines};
+
+fn main() {
+    let input = read_to_string("input.txt").expect("no input.txt in current directory");
+    let input: Vec<_> = input.trim().lines().collect();
+    let numbers = parse_lines(&input).unwrap();
+    let checksum = compute_checksum(&numbers).unwrap();
+    println!("Checksum: {checksum}");
+}
