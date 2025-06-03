ce43cb0f8ac57ab3fc8f4790ed9e99a89bd5b419
msharipov <msharipovr@gmail.com>
2025-06-03 12:26:11 -0400

    (17_2_2): completed

--- 2017/day2_2/src/main.rs
+++ 2017/day2_2/src/main.rs
@@ -1,11 +1,11 @@
 use std::fs::read_to_string;
 
-use day2_2::{compute_checksum, parse_lines};
+use day2_2::{compute_checksum_2, parse_lines};
 
 fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
     let input: Vec<_> = input.trim().lines().collect();
     let numbers = parse_lines(&input).unwrap();
-    let checksum = compute_checksum(&numbers).unwrap();
+    let checksum = compute_checksum_2(&numbers).unwrap();
     println!("Checksum: {checksum}");
 }
