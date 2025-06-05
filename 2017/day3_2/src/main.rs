8ba28e2d8ef30e051b6e5f8b8d8380a5d47a5af2
msharipov <msharipovr@gmail.com>
2025-06-05 00:03:52 -0400

    (17_3_2): completed

--- 2017/day3_2/src/main.rs
+++ 2017/day3_2/src/main.rs
@@ -1,11 +1,10 @@
 use std::fs::read_to_string;
 
-use day3_2::{Offset, manhattan_distance, offset};
+use day3_2::first_value_larger_than;
 
 fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
     let input: u64 = input.trim().parse().unwrap();
-    let position = offset(input.try_into().unwrap());
-    let steps = manhattan_distance(&Offset { right: 0, up: 0 }, &position);
-    println!("{steps} steps");
+    let value = first_value_larger_than(input);
+    println!("{value}");
 }
