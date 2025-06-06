f49dd338b3012bfb92e0845e4bdc7123f72cf7a0
msharipov <msharipovr@gmail.com>
2025-06-06 12:47:59 -0400

    (17_4_2): count letters in each word

---
+++ 2017/day4_2/src/lib.rs
@@ -0,0 +1,63 @@
+use std::collections::{BTreeMap, HashSet};
+
+fn letter_counts(word: &str) -> BTreeMap<char, usize> {
+    let mut counts = BTreeMap::new();
+    for c in word.chars() {
+        match counts.remove(&c) {
+            None => counts.insert(c, 1),
+            Some(old) => counts.insert(c, old + 1),
+        };
+    }
+    counts
+}
+
+pub fn validate_passphrase(passphrase: &str) -> bool {
+    let mut found_words = HashSet::new();
+    passphrase.split(' ').all(|word| found_words.insert(word))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn validate_passphrase_test_1() {
+        assert!(validate_passphrase("abcdefg"));
+    }
+
+    #[test]
+    fn validate_passphrase_test_2() {
+        assert!(validate_passphrase("aa bb cc dd ee ff"));
+    }
+
+    #[test]
+    fn validate_passphrase_test_3() {
+        assert!(!validate_passphrase("aa bb cc aa dd ee ff"));
+    }
+
+    #[test]
+    fn validate_passphrase_test_4() {
+        assert!(validate_passphrase("aa bb cc AA dd ee ff"));
+    }
+
+    #[test]
+    fn letter_counts_test_1() {
+        assert_eq!(letter_counts(""), BTreeMap::new());
+    }
+
+    #[test]
+    fn letter_counts_test_2() {
+        assert_eq!(
+            letter_counts("abcd"),
+            BTreeMap::from_iter([('a', 1), ('b', 1), ('c', 1), ('d', 1)])
+        );
+    }
+
+    #[test]
+    fn letter_counts_test_3() {
+        assert_eq!(
+            letter_counts("aabbbbcddd"),
+            BTreeMap::from_iter([('a', 2), ('b', 4), ('c', 1), ('d', 3)])
+        );
+    }
+}
---
+++ 2017/day4_2/src/main.rs
@@ -0,0 +1,13 @@
+use std::fs::read_to_string;
+
+use day4_1::validate_passphrase;
+
+fn main() {
+    let input = read_to_string("input.txt").expect("no input.txt in current directory");
+    let input: Vec<_> = input.trim().lines().collect();
+    let valid_count = input
+        .iter()
+        .filter(|phrase| validate_passphrase(phrase))
+        .count();
+    println!("{valid_count} valid pass phrases");
+}
