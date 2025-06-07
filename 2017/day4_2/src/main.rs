89d76ee9519147810e2a97244c66f9ea731674c9
msharipov <msharipovr@gmail.com>
2025-06-07 08:54:26 -0400

    (17_4_2): completed

--- 2017/day4_2/src/main.rs
+++ 2017/day4_2/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs::read_to_string;
 
-use day4_1::validate_passphrase;
+use day4_2::validate_passphrase;
 
 fn main() {
     let input = read_to_string("input.txt").expect("no input.txt in current directory");
