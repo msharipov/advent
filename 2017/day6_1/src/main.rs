7c64d6857639e46cd7b421c763ca553f036fca48
msharipov <msharipovr@gmail.com>
2025-06-12 02:35:15 -0400

    (17_6_1): completed

--- 2017/day6_1/src/main.rs
+++ 2017/day6_1/src/main.rs
@@ -1,3 +1,10 @@
+use std::fs::read_to_string;
+
+use day6_1::Memory;
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_to_string("input.txt").expect("no input.txt in current directory");
+    let input: Vec<_> = input.trim().lines().collect();
+    let mem = input[0].parse::<Memory>().unwrap();
+    println!("{} redistribution cycles", mem.cycle_period());
 }
