57266f49cdded538c799c7c8e07e510152dff394
msharipov <msharipovr@gmail.com>
2025-07-09 04:40:27 -0400

    (17_9_1): parse garbage

---
+++ 2017/day9_1/src/lib.rs
@@ -0,0 +1,77 @@
+use std::str::FromStr;
+
+use thiserror::Error;
+
+#[derive(Debug, PartialEq)]
+pub enum GroupPart {
+    Group(Vec<GroupPart>),
+    Garbage,
+}
+
+#[derive(Debug, Error, PartialEq)]
+#[error("failed to parse group")]
+pub struct GroupParseError();
+
+fn parse_garbage(s: &str) -> Result<GroupPart, GroupParseError> {
+    let chars: Vec<_> = s.chars().collect();
+    let len = chars.len();
+    if chars[0] != '<' || len < 2 {
+        return Err(GroupParseError());
+    }
+    let mut current_index: usize = 0;
+    let mut escaped = false;
+    loop {
+        current_index += 1;
+        if current_index >= len {
+            return Err(GroupParseError());
+        }
+        if escaped {
+            escaped = false;
+            continue;
+        }
+        match chars[current_index] {
+            '!' => {
+                escaped = true;
+            }
+            '>' if !escaped => {
+                break;
+            }
+            _ => {}
+        }
+    }
+    if current_index != len - 1 {
+        return Err(GroupParseError());
+    }
+    Ok(GroupPart::Garbage)
+}
+
+impl FromStr for GroupPart {
+    type Err = GroupParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.is_empty() {
+            return Err(GroupParseError());
+        }
+        let chars: Vec<_> = s.chars().collect();
+        if chars[0] == '<' {
+            parse_garbage(s)
+        } else {
+            Err(GroupParseError())
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn grouppart_parse_test_1() {
+        assert_eq!("<>".parse(), Ok(GroupPart::Garbage));
+    }
+
+    #[test]
+    fn grouppart_parse_test_2() {
+        assert_eq!("".parse::<GroupPart>(), Err(GroupParseError()));
+    }
+}
